4fa530bbb907af0ee8a5c723a5b242157a13ef13
Priyanka Nehra <priyankanehra@Priyankas-MacBook-Pro.local>
2023-07-22 15:48:05 -0400

    module add

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Contact from './Pages/Contact.js'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './Pages/Home.js'
 import Design from './Pages/Design.js'
+import { createBrowserHistory } from "history"
+
 
 
 
 function App() {
+  const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL,
+  });
+
   return (
     <div className="container"> 
         <Router>
           <Header/>
-          <Routes basename={process.env.PUBLIC_URL}>
+          <Routes basename={history}>
             <Route path="/" element={<Home/>}/>
             <Route path='/contact' element={<Contact />} />
             <Route path='/design' element={<Design />} />
