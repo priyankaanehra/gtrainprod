829dec90a823e1b6ed68d35f08c5b6f69decbfb0
priyankaanehra <priyankaanehra@gmail.com>
2024-01-13 22:23:13 -0500

    sizing trial

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,21 +1,21 @@
 import './Home.css'
-import bgvid from "../../assets/gtrainvid1.mp4"
+import bgvid from "../../assets/gtrainvid2.mp4"
 
 export default function Home(){
 
     return(
         <div>
-        <div>
-            <div className='gtvideo'>
-            <video autoPlay muted loop id="myVideo">
-                <source src={bgvid} type="video/mp4"/>
-            </video>
-            </div>
-            <div className='gname'>
-                <h1 className='homeName'>G-TRAIN PRODUCTIONS</h1>
-                <div className='horizontalblock'></div>
-                <p className='companytype'>Production Company</p>
-            </div>    
+            <div>
+                <div className='gtvideo'>
+                <video autoPlay muted loop id="myVideo">
+                    <source src={bgvid} type="video/mp4"/>
+                </video>
+                </div>
+                <div className='gname'>
+                    <h1 className='homeName'>G-TRAIN PRODUCTIONS</h1>
+                    <div className='horizontalblock'></div>
+                    <p className='companytype'>Production Company</p>
+                </div>    
             </div>       
         </div>
         )
